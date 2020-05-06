0c4a8d340672a6fb661324ccaa9bfdd2de60a546
erhan-er <erhan.er@ug.bilkent.edu.tr>
2020-05-06 22:18:12 +0300

    Erhan,Berk- Entrance Pages Final

--- EntrancePages/app/src/main/java/com/example/entrancepages/UserProfile.java
+++ EntrancePages/app/src/main/java/com/example/entrancepages/UserProfile.java
@@ -1,14 +1,41 @@
 package com.example.entrancepages;
 
+/**
+ * Simple java class
+ */
 public class UserProfile {
     public String userName;
     public String userEmail;
     public int bilcoin;
 
-    public UserProfile( String userName, String userEmail )
-    {
+    // constructors
+    public UserProfile() {
+
+    }
+
+    public UserProfile(String userName, String userEmail) {
         this.userName = userName;
         this.userEmail = userEmail;
         bilcoin = 0;
     }
+
+    // methods
+
+    /**
+     *  gets the userName
+     * @return userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * gets the bilcoin
+     * @return bilcoin
+     */
+    public int getBilcoin()
+    {
+        return bilcoin;
+    }
+
 }
